38021ae2a1fe5b812a2adc4f42d477e15afd37d4
skidd-level-100 <bumpsh@protonmail.com>
2026-06-22 20:32:42 -0700

    add cava and rice_and_fun package sections

--- setup-settings.sh
+++ setup-settings.sh
@@ -5,10 +5,11 @@ gui_pkgs="gui-wm/hyprland gui-libs/xdg-desktop-portal-hyprland sys-apps/xdg-desk
 tui_pkgs="net-im/profanity net-irc/irssi"
 sound_pkgs="media-video/wireplumber"
 networking_pkgs="net-misc/networkmanager net-p2p/syncthing net-p2p/yggdrasil-go net-firewall/nftables"
-tools_pkgs="sys-fs/cryptsetup app-portage/eix sys-power/tlp app-admin/eclean-kernel net-misc/chrony app-shells/zsh app-shells/zsh-completions app-shells/zsh-syntax-highlighting app-containers/podman app-containers/podman-compose sys-apps/flatpak gui-apps/wl-clipboard xfce-base/thunar-volman dev-util/rocm-smi dev-python/pip app-misc/fastfetch"
+tools_pkgs="sys-fs/cryptsetup app-portage/eix sys-power/tlp app-admin/eclean-kernel net-misc/chrony app-shells/zsh app-shells/zsh-completions app-containers/podman app-containers/podman-compose sys-apps/flatpak gui-apps/wl-clipboard xfce-base/thunar-volman dev-util/rocm-smi dev-python/pip"
 misc_pkgs="app-shells/bash-completion media-fonts/nerdfonts media-fonts/fontawesome sys-block/io-scheduler-udev-rules sys-fs/dosfstools sys-fs/xfsprogs sys-auth/rtkit"
+rice_and_fun="media-sound/cava app-misc/fastfetch app-shells/zsh-syntax-highlighting"
 
-additional_pkgs="$kernel_pkgs $tui_pkgs $gui_pkgs $sound_pkgs $networking_pkgs $tools_pkgs $misc_pkgs $kernel_comp"
+additional_pkgs="$kernel_pkgs $tui_pkgs $gui_pkgs $sound_pkgs $networking_pkgs $tools_pkgs $misc_pkgs $rice_and_fun $kernel_comp"
 
 ## Names
 username=""
