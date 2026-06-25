3168c4c4a484df11026820dd6da29c6dd9ec985a
skidd-level-100 <bumpsh@protonmail.com>
2026-06-24 23:32:13 -0700

    add terminal lol

--- setup-settings.sh
+++ setup-settings.sh
@@ -1,7 +1,7 @@
 # settings!
 kernel_pkgs="sys-kernel/gentoo-sources sys-kernel/ugrd"
 kernel_comp="llvm-core/lld llvm-core/clang"
-gui_pkgs="gui-wm/hyprland gui-libs/xdg-desktop-portal-hyprland sys-apps/xdg-desktop-portal-gtk gui-libs/hyprland-guiutils sys-auth/hyprpolkitagent gui-apps/hyprshot gui-apps/hyprpwcenter gui-apps/hyprpaper gui-apps/hyprmoncfg gui-apps/hyprlock gui-apps/waybar gui-apps/wofi gui-apps/waybar gui-apps/mako xfce-base/thunar xfce-extra/thunar-archive-plugin app-arch/file-roller gui-apps/mako media-video/mpv media-gfx/imv"
+gui_pkgs="gui-wm/hyprland gui-libs/xdg-desktop-portal-hyprland sys-apps/xdg-desktop-portal-gtk gui-libs/hyprland-guiutils sys-auth/hyprpolkitagent gui-apps/hyprshot gui-apps/hyprpwcenter gui-apps/hyprpaper gui-apps/hyprmoncfg gui-apps/hyprlock gui-apps/waybar gui-apps/wofi gui-apps/waybar gui-apps/mako xfce-base/thunar xfce-extra/thunar-archive-plugin app-arch/file-roller gui-apps/mako media-video/mpv media-gfx/imv gui-apps/foot"
 tui_pkgs="net-im/profanity net-irc/irssi"
 sound_pkgs="media-video/wireplumber"
 networking_pkgs="net-misc/networkmanager net-p2p/syncthing net-p2p/yggdrasil-go net-firewall/nftables"
